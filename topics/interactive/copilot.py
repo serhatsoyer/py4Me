d4528481e61fdfd9a2f712892f764c503f0e24e1
M. Serhat Soyer <60302475+serhatsoyer@users.noreply.github.com>
2023-03-16 11:53:07 +0000

    copilot introduced

---
+++ topics/interactive/copilot.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Create a zeros numpy array of size 5:
+a = np.zeros(5)
+
+# Create a zeros numpy array of size 5x5:
+b = np.zeros((5,5))
+
+# Create a zeros numpy array of size 5x5x5:
+c = np.zeros((5,5,5))
+
+# Tab to autocomplete
+
+# control + Enter to open a Copilot window
+
+# Escape to cancel Copilot
+
+# option + N: next Copilot recommendation
+# option + P: previous Copilot recommendation
